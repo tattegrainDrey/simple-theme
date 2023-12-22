1168d775235693585f16ed2e440a40fea91d5905
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2023-12-21 21:58:23 -0500

    fin de single
    fin du html et sass pour single de portfolio

--- single.php
+++ single.php
@@ -1,3 +1,29 @@
 <?php get_header(); ?>
-<pre>single.php</pre>
+<main>
+    <section class="single">
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post();
+                if (in_category('portfolio')) : ?>
+                    <div class="content">
+                        <div class="portfolio item">
+                            <?php
+                            the_title('<h3>', '</h3>');
+                            the_post_thumbnail();
+                            the_content();
+                            ?>
+                        </div>
+                        <a href="../" class="retour">retour</a>
+                    </div>
+                <?php else :
+                    the_title('<h1>', '</h1>');
+                    the_content();
+                endif;
+                ?>
+                <!--pourquoi le hr? -->
+                <hr>
+        <?php endwhile;
+        endif;
+        ?>
+    </section>
+</main>
 <?php get_footer(); ?>
