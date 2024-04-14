5e3f5453ca5aee198cbe2e9e75c78dfea468a2b4
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-04-14 02:48:26 +0000

    onclick icons

--- header.php
+++ header.php
@@ -9,18 +9,47 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+<script>
+    function magnifier() {
+        let ham = document.getElementById('hamburger')
+        let  mag = document.getElementById('search')
+        if (ham.style.display == "flex") {
+            ham.style.display = "none"
+            mag.style.display = "flex"
+        }
+        else {
+            mag.style.display == "flex" ? mag.style.display = "none" : mag.style.display = "flex"
+        }
+    }
+    function hamburger() {
+        let ham = document.getElementById('hamburger')
+        let  mag = document.getElementById('search')
+        if (mag.style.display == "flex") {
+            mag.style.display = "none"
+            ham.style.display = "flex"
+        }
+        else {
+            ham.style.display == "flex" ? ham.style.display = "none" : ham.style.display = "flex"
+        }
+    }
+</script>
 </head>
 
 <body>
     <header>
         <h1><a href="<?php get_site_url() ?>"><?php bloginfo('name') ?></a></h1>
         <div>
-            <img src="https://audreytattegrain.ca/wp-content/uploads/2024/04/menu.svg" alt="search icon" class="icon">
-            <img src="https://audreytattegrain.ca/wp-content/uploads/2024/04/search.svg" alt="menu icon" class="icon">
+            <img onclick="magnifier()" src="https://audreytattegrain.ca/wp-content/uploads/2024/04/search.svg" alt="search icon" class="icon">
+            <img onclick="hamburger()" src="https://audreytattegrain.ca/wp-content/uploads/2024/04/menu.svg" alt="menu icon" class="icon">
             <form action="" id="search">
                 <input type="search" name="search">
                 <input type="image" src="https://audreytattegrain.ca/wp-content/uploads/2024/04/send.svg">
             </form>
-            <?php wp_nav_menu('main-menu') ?> 
+            <?php wp_nav_menu(array(
+                'menu'                 => 'main-menu',
+                'container'            => 'ul',
+                'container_class'      => 'bigScreen',
+                'container_id'         => 'hamburger'
+            )) ?> 
         </div>
     </header>
