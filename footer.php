e5065020527fb95bca9846d094c761ac9879bc96
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-04-14 03:09:20 +0000

    moving script

--- footer.php
+++ footer.php
@@ -4,4 +4,29 @@
 <?php wp_footer(); ?>
 </body>
 
+<script>
+    function magnifier() {
+        let ham = document.getElementById('hamburger');
+        let  mag = document.getElementById('search');
+        if (ham.style.display == "flex") {
+            ham.style.display = "none";
+            mag.style.display = "flex";
+        }
+        else {
+            mag.style.display == "flex" ? mag.style.display = "none" : mag.style.display = "flex";
+        }
+    }
+    function hamburger() {
+        let ham = document.getElementById('hamburger');
+        let  mag = document.getElementById('search');
+        if (mag.style.display == "flex") {
+            mag.style.display = "none";
+            ham.style.display = "flex";
+        }
+        else {
+            ham.style.display == "flex" ? ham.style.display = "none" : ham.style.display = "flex";
+        }
+    }
+</script>
+
 </html>
--- header.php
+++ header.php
@@ -29,27 +29,3 @@
             )) ?> 
         </div>
     </header>
-    <script>
-    function magnifier() {
-        let ham = document.getElementById('hamburger')
-        let  mag = document.getElementById('search')
-        if (ham.style.display == "flex") {
-            ham.style.display = "none"
-            mag.style.display = "flex"
-        }
-        else {
-            mag.style.display == "flex" ? mag.style.display = "none" : mag.style.display = "flex"
-        }
-    }
-    function hamburger() {
-        let ham = document.getElementById('hamburger')
-        let  mag = document.getElementById('search')
-        if (mag.style.display == "flex") {
-            mag.style.display = "none"
-            ham.style.display = "flex"
-        }
-        else {
-            ham.style.display == "flex" ? ham.style.display = "none" : ham.style.display = "flex"
-        }
-    }
-</script>
