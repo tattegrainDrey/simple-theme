3291b7f0bd9c0438333643854b01e59fa414d86a
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-04-17 01:46:06 +0000

    fixing front page display?

--- front-page.php
+++ front-page.php
@@ -1,7 +1,16 @@
 <?php get_header() ?>
 <main class="front">
     <?php
-        echo apply_filters( 'the_content', get_post('intro')->post_content );
+    $args = array(
+        'post_name'      => 'intro',
+        'post_type'      => 'post',
+        'post_status'    => 'publish',
+        'posts_per_page' => 1
+    );
+    $my_posts = get_posts($args);
+    if ($my_posts) :
+        echo 'ID on the first post found ' . $my_posts[0]->post_content;
+    endif;
     ?>
 </main>
 
